9b81395c57ca961f09badd90baa7fb3a9250dc3c
oksana.labeiko <oksana.labeiko@itexus.com>
2022-01-20 14:11:19 +0300

    N.17 reduce: fix

--- N.17/reduce/index.js
+++ N.17/reduce/index.js
@@ -2,14 +2,13 @@ let message = prompt("Введите строку", "");
 
 let getSum = (str) => {
   let vowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
-  let sum = 0;
   str = str.toLowerCase().split("");
   return str.reduce((prev, cur) => {
     if (vowels.indexOf(cur) >= 0) {
-      prev = ++sum;
+      prev+=1;
     }
     return prev;
-  }, sum);
+  }, 0);
 };
 
 alert(`В этой строке ${getSum(message)} строчные/х букв`);
